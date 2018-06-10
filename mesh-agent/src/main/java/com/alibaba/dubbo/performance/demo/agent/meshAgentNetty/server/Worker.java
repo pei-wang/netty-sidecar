d253aecf1ea1e32ef96a8e9398227a65b05d0850
Wang pei <519875872@qq.com>
2018-06-10 23:47:27 +0800

    tunning

--- mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/meshAgentNetty/server/Worker.java
+++ mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/meshAgentNetty/server/Worker.java
@@ -10,7 +10,7 @@ public class Worker extends Thread {
         private static final BlockingQueue<Runnable> tasks = new LinkedBlockingQueue<>();
 //    private static final SynchronousQueue<Runnable> tasks = new SynchronousQueue<>();
 
-        public static ExecutorService executorService = new ThreadPoolExecutor(50, 80, 1000L, TimeUnit.MILLISECONDS, tasks);
+        public static ExecutorService executorService = new ThreadPoolExecutor(50, 80, 100L, TimeUnit.MILLISECONDS, tasks);
 //    public static ExecutorService executorService = new ThreadPoolExecutor(80, Integer.MAX_VALUE, 100L, TimeUnit.MILLISECONDS, tasks);
     private static long startTime = System.currentTimeMillis();
 
