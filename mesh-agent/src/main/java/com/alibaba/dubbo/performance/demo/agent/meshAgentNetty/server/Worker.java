360cb3ed30686dce0cbfbc59882115b94023c750
Wang pei <519875872@qq.com>
2018-06-10 22:57:03 +0800

    tunning

--- mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/meshAgentNetty/server/Worker.java
+++ mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/meshAgentNetty/server/Worker.java
@@ -10,14 +10,14 @@ public class Worker extends Thread {
         private static final BlockingQueue<Runnable> tasks = new LinkedBlockingQueue<>();
 //    private static final SynchronousQueue<Runnable> tasks = new SynchronousQueue<>();
 
-        public static ExecutorService executorService = new ThreadPoolExecutor(80, 150, 1000L, TimeUnit.MILLISECONDS, tasks);
+        public static ExecutorService executorService = new ThreadPoolExecutor(50, 80, 1000L, TimeUnit.MILLISECONDS, tasks);
 //    public static ExecutorService executorService = new ThreadPoolExecutor(80, Integer.MAX_VALUE, 100L, TimeUnit.MILLISECONDS, tasks);
     private static long startTime = System.currentTimeMillis();
 
     public static void dispatch(DBHandler dbHandler) {
         executorService.execute(dbHandler);
+        LOGGER.info("current tasks size:" + tasks.size());
         if ((System.currentTimeMillis() - startTime) > 1000) {
-            LOGGER.info("current tasks size:" + tasks.size());
             long freeMemory = Runtime.getRuntime().freeMemory() / 1024 / 1024;//已使用内存
             long totalMemory = Runtime.getRuntime().totalMemory() / 1024 / 1024;//总共可使用内存
             System.out.println("可用处理器：" + Runtime.getRuntime().availableProcessors());
